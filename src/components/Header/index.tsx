5d08352c39162866dc8ad6abfbe4aab64ef88740
Eduardo-DevJs <dudev2001@gmail.com>
2024-04-29 11:24:19 -0300

    Feat - Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href={"/"}>
+            <h1 className={styles.logo}>
+              Tarefas <span>+</span>
+            </h1>
+          </Link>
+          <Link className={styles.link} href={"/dashboard"}>Meu painel</Link>
+        </nav>
+        <button className={styles.loginButton}>Acessar</button>
+      </section>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,15 @@ export default function Home() {
         <h1 className={styles.title}>
           Sistema feito para você organizar <br /> seus estudos e tarefas
         </h1>
+        
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
       </main>
     </div>
   );
