94300dd9902a4c51b2eaa06021dc6c6a094cdad5
Eduardo-DevJs <dudev2001@gmail.com>
2024-04-29 12:31:10 -0300

    feat - Rota Dashboard privada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import styles from "./styles.module.css"
-import Head from "next/head"
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
@@ -9,5 +11,21 @@ export default function Dashboard() {
       </Head>
       <h1>Página painel</h1>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  // console.log(session)
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
